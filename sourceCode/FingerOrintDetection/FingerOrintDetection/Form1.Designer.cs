e867307ea7ebbb17d712acf7477e5d1efd6886b2
congnguyen-framas <68525123+congnguyen-framas@users.noreply.github.com>
2022-11-22 18:17:50 +0700

    UP OK

---
+++ sourceCode/FingerOrintDetection/FingerOrintDetection/Form1.Designer.cs
@@ -0,0 +1,237 @@
+﻿
+using System;
+
+namespace FingerOrintDetection
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.bnInit = new System.Windows.Forms.Button();
+            this.bnOpen = new System.Windows.Forms.Button();
+            this.bnEnroll = new System.Windows.Forms.Button();
+            this.bnVerify = new System.Windows.Forms.Button();
+            this.bnFree = new System.Windows.Forms.Button();
+            this.bnClose = new System.Windows.Forms.Button();
+            this.bnIdentify = new System.Windows.Forms.Button();
+            this.textRes = new System.Windows.Forms.TextBox();
+            this.picFPImg = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.cmbIdx = new System.Windows.Forms.ComboBox();
+            this.txtFingerprintCode = new System.Windows.Forms.TextBox();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.btnAddFinger = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.picFPImg)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // bnInit
+            // 
+            this.bnInit.Location = new System.Drawing.Point(27, 29);
+            this.bnInit.Name = "bnInit";
+            this.bnInit.Size = new System.Drawing.Size(75, 25);
+            this.bnInit.TabIndex = 0;
+            this.bnInit.Text = "Initialize";
+            this.bnInit.UseVisualStyleBackColor = true;
+            this.bnInit.Click += new System.EventHandler(this.bnInit_Click);
+            // 
+            // bnOpen
+            // 
+            this.bnOpen.Enabled = false;
+            this.bnOpen.Location = new System.Drawing.Point(27, 77);
+            this.bnOpen.Name = "bnOpen";
+            this.bnOpen.Size = new System.Drawing.Size(75, 25);
+            this.bnOpen.TabIndex = 1;
+            this.bnOpen.Text = "Open";
+            this.bnOpen.UseVisualStyleBackColor = true;
+            this.bnOpen.Click += new System.EventHandler(this.bnOpen_Click);
+            // 
+            // bnEnroll
+            // 
+            this.bnEnroll.Enabled = false;
+            this.bnEnroll.Location = new System.Drawing.Point(27, 121);
+            this.bnEnroll.Name = "bnEnroll";
+            this.bnEnroll.Size = new System.Drawing.Size(75, 25);
+            this.bnEnroll.TabIndex = 2;
+            this.bnEnroll.Text = "Enroll";
+            this.bnEnroll.UseVisualStyleBackColor = true;
+            this.bnEnroll.Click += new System.EventHandler(this.bnEnroll_Click);
+            // 
+            // bnVerify
+            // 
+            this.bnVerify.Enabled = false;
+            this.bnVerify.Location = new System.Drawing.Point(27, 164);
+            this.bnVerify.Name = "bnVerify";
+            this.bnVerify.Size = new System.Drawing.Size(75, 25);
+            this.bnVerify.TabIndex = 3;
+            this.bnVerify.Text = "Verify";
+            this.bnVerify.UseVisualStyleBackColor = true;
+            this.bnVerify.Click += new System.EventHandler(this.bnVerify_Click);
+            // 
+            // bnFree
+            // 
+            this.bnFree.Enabled = false;
+            this.bnFree.Location = new System.Drawing.Point(130, 29);
+            this.bnFree.Name = "bnFree";
+            this.bnFree.Size = new System.Drawing.Size(75, 25);
+            this.bnFree.TabIndex = 4;
+            this.bnFree.Text = "Finalize";
+            this.bnFree.UseVisualStyleBackColor = true;
+            this.bnFree.Click += new System.EventHandler(this.bnFree_Click);
+            // 
+            // bnClose
+            // 
+            this.bnClose.Enabled = false;
+            this.bnClose.Location = new System.Drawing.Point(130, 77);
+            this.bnClose.Name = "bnClose";
+            this.bnClose.Size = new System.Drawing.Size(75, 25);
+            this.bnClose.TabIndex = 5;
+            this.bnClose.Text = "Close";
+            this.bnClose.UseVisualStyleBackColor = true;
+            this.bnClose.Click += new System.EventHandler(this.bnClose_Click);
+            // 
+            // bnIdentify
+            // 
+            this.bnIdentify.Enabled = false;
+            this.bnIdentify.Location = new System.Drawing.Point(130, 121);
+            this.bnIdentify.Name = "bnIdentify";
+            this.bnIdentify.Size = new System.Drawing.Size(75, 25);
+            this.bnIdentify.TabIndex = 6;
+            this.bnIdentify.Text = "Identiy";
+            this.bnIdentify.UseVisualStyleBackColor = true;
+            this.bnIdentify.Click += new System.EventHandler(this.bnIdentify_Click);
+            // 
+            // textRes
+            // 
+            this.textRes.Location = new System.Drawing.Point(1, 200);
+            this.textRes.Multiline = true;
+            this.textRes.Name = "textRes";
+            this.textRes.ReadOnly = true;
+            this.textRes.Size = new System.Drawing.Size(766, 70);
+            this.textRes.TabIndex = 7;
+            // 
+            // picFPImg
+            // 
+            this.picFPImg.Location = new System.Drawing.Point(427, 15);
+            this.picFPImg.Name = "picFPImg";
+            this.picFPImg.Size = new System.Drawing.Size(131, 174);
+            this.picFPImg.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.picFPImg.TabIndex = 8;
+            this.picFPImg.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(210, 35);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(36, 13);
+            this.label1.TabIndex = 9;
+            this.label1.Text = "Index:";
+            // 
+            // cmbIdx
+            // 
+            this.cmbIdx.FormattingEnabled = true;
+            this.cmbIdx.Location = new System.Drawing.Point(248, 31);
+            this.cmbIdx.Name = "cmbIdx";
+            this.cmbIdx.Size = new System.Drawing.Size(40, 21);
+            this.cmbIdx.TabIndex = 10;
+            // 
+            // txtFingerprintCode
+            // 
+            this.txtFingerprintCode.Location = new System.Drawing.Point(1, 276);
+            this.txtFingerprintCode.Multiline = true;
+            this.txtFingerprintCode.Name = "txtFingerprintCode";
+            this.txtFingerprintCode.ReadOnly = true;
+            this.txtFingerprintCode.Size = new System.Drawing.Size(766, 435);
+            this.txtFingerprintCode.TabIndex = 7;
+            // 
+            // btnClear
+            // 
+            this.btnClear.Enabled = false;
+            this.btnClear.Location = new System.Drawing.Point(130, 164);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(75, 25);
+            this.btnClear.TabIndex = 3;
+            this.btnClear.Text = "ClearAll";
+            this.btnClear.UseVisualStyleBackColor = true;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+            // 
+            // btnAddFinger
+            // 
+            this.btnAddFinger.Enabled = false;
+            this.btnAddFinger.Location = new System.Drawing.Point(236, 164);
+            this.btnAddFinger.Name = "btnAddFinger";
+            this.btnAddFinger.Size = new System.Drawing.Size(110, 25);
+            this.btnAddFinger.TabIndex = 5;
+            this.btnAddFinger.Text = "Add Fingerprint test";
+            this.btnAddFinger.UseVisualStyleBackColor = true;
+            this.btnAddFinger.Click += new System.EventHandler(this.btnAddFinger_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(779, 723);
+            this.Controls.Add(this.cmbIdx);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.picFPImg);
+            this.Controls.Add(this.txtFingerprintCode);
+            this.Controls.Add(this.textRes);
+            this.Controls.Add(this.bnIdentify);
+            this.Controls.Add(this.btnAddFinger);
+            this.Controls.Add(this.bnClose);
+            this.Controls.Add(this.bnFree);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.bnVerify);
+            this.Controls.Add(this.bnEnroll);
+            this.Controls.Add(this.bnOpen);
+            this.Controls.Add(this.bnInit);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.picFPImg)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button bnInit;
+        private System.Windows.Forms.Button bnOpen;
+        private System.Windows.Forms.Button bnEnroll;
+        private System.Windows.Forms.Button bnVerify;
+        private System.Windows.Forms.Button bnFree;
+        private System.Windows.Forms.Button bnClose;
+        private System.Windows.Forms.Button bnIdentify;
+        private System.Windows.Forms.TextBox textRes;
+        private System.Windows.Forms.PictureBox picFPImg;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox cmbIdx;
+        private System.Windows.Forms.TextBox txtFingerprintCode;
+        private System.Windows.Forms.Button btnClear;
+        private System.Windows.Forms.Button btnAddFinger;
+    }
+}
+
---
+++ sourceCode/FingerOrintDetection/FingerOrintDetection/Form1.cs
@@ -0,0 +1,387 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+//using ImageProcessor.Imaging.Formats;
+using libzkfpcsharp;
+
+
+namespace FingerOrintDetection
+{
+    public partial class Form1 : Form
+    {
+        IntPtr mDevHandle = IntPtr.Zero;
+        IntPtr mDBHandle = IntPtr.Zero;
+        IntPtr FormHandle = IntPtr.Zero;
+        bool bIsTimeToDie = false;
+        bool IsRegister = false;
+        bool bIdentify = true;
+        byte[] FPBuffer;//chứa mã code base64 của fingerrint
+        byte[] FPBufferTest;//chứa mã code base64 của fingerrint
+
+        int RegisterCount = 0;
+        const int REGISTER_FINGER_COUNT = 3;
+
+        byte[][] RegTmps = new byte[3][];
+        byte[] RegTmp = new byte[2048];
+        byte[] CapTmp = new byte[2048];
+        int cbCapTmp = 2048;
+        int cbRegTmp = 0;
+        int iFid = 1;//define Id cho fingerprint. băt đầu từ 1
+        Thread captureThread = null;
+
+        private int mfpWidth = 0;
+        private int mfpHeight = 0;
+
+        const int MESSAGE_CAPTURED_OK = 0x0400 + 6;
+
+        [DllImport("user32.dll", EntryPoint = "SendMessageA")]
+        public static extern int SendMessage(IntPtr hwnd, int wMsg, IntPtr wParam, IntPtr lParam);
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            Load += Form1_Load;
+        }
+
+        /// <summary>
+        /// Method Reading fingerprint.
+        /// </summary>
+        private void DoCapture()
+        {
+            while (!bIsTimeToDie)
+            {
+                cbCapTmp = 2048;
+                int ret = zkfp2.AcquireFingerprint(mDevHandle, FPBuffer, CapTmp, ref cbCapTmp);
+                if (ret == zkfp.ZKFP_ERR_OK)
+                {
+                    SendMessage(FormHandle, MESSAGE_CAPTURED_OK, IntPtr.Zero, IntPtr.Zero);
+                }
+                Thread.Sleep(200);
+            }
+        }
+
+        protected override void DefWndProc(ref Message m)
+        {
+            switch (m.Msg)
+            {
+                case MESSAGE_CAPTURED_OK:
+                    {
+                        MemoryStream ms = new MemoryStream();
+                        BitmapFormat.GetBitmap(FPBuffer, mfpWidth, mfpHeight, ref ms);
+                        Bitmap bmp = new Bitmap(ms);
+                        this.picFPImg.Image = bmp;
+
+                        if (IsRegister)
+                        {
+                            int ret = zkfp.ZKFP_ERR_OK;
+                            int fid = 0, score = 0;
+                            ret = zkfp2.DBIdentify(mDBHandle, CapTmp, ref fid, ref score);
+                            if (zkfp.ZKFP_ERR_OK == ret)
+                            {
+                                textRes.Text = "This finger was already register by " + fid + "!";
+                                return;
+                            }
+                            if (RegisterCount > 0 && zkfp2.DBMatch(mDBHandle, CapTmp, RegTmps[RegisterCount - 1]) <= 0)
+                            {
+                                textRes.Text = "Please press the same finger 3 times for the enrollment";
+                                return;
+                            }
+                            Array.Copy(CapTmp, RegTmps[RegisterCount], cbCapTmp);
+                            String strBase64 = zkfp2.BlobToBase64(CapTmp, cbCapTmp);
+                            byte[] blob = zkfp2.Base64ToBlob(strBase64);
+                            RegisterCount++;
+
+                            if (RegisterCount >= REGISTER_FINGER_COUNT)
+                            {
+                                RegisterCount = 0;
+                                if (zkfp.ZKFP_ERR_OK == (ret = zkfp2.DBMerge(mDBHandle, RegTmps[0], RegTmps[1], RegTmps[2], RegTmp, ref cbRegTmp)) &&
+                                       zkfp.ZKFP_ERR_OK == (ret = zkfp2.DBAdd(mDBHandle, iFid, RegTmp)))
+                                {
+                                    iFid++;
+                                    textRes.Text = $"enroll succ";
+                                    txtFingerprintCode.Text = $"Fingerprint:{Environment.NewLine}{zkfp2.BlobToBase64(RegTmp, cbRegTmp)}";
+                                }
+                                else
+                                {
+                                    textRes.Text = "enroll fail, error code=" + ret;
+                                }
+                                IsRegister = false;
+                                return;
+                            }
+                            else
+                            {
+                                textRes.Text = "You need to press the " + (REGISTER_FINGER_COUNT - RegisterCount) + " times fingerprint";
+                            }
+                        }
+                        else
+                        {
+                            if (cbRegTmp <= 0)
+                            {
+                                textRes.Text = "Please register your finger first!";
+                                return;
+                            }
+                            if (bIdentify)//Xác thực vân tay
+                            {
+                                int ret = zkfp.ZKFP_ERR_OK;
+                                int fid = 0, score = 0;
+                                ret = zkfp2.DBIdentify(mDBHandle, CapTmp, ref fid, ref score);
+                                if (zkfp.ZKFP_ERR_OK == ret)
+                                {
+                                    textRes.Text = "Identify succ, fid= " + fid + ",score=" + score + "!";
+                                    return;
+                                }
+                                else
+                                {
+                                    textRes.Text = "Identify fail, ret= " + ret;
+                                    return;
+                                }
+                            }
+                            else//Kiểm chứng lại vân tay(verify)
+                            {
+                                int ret = zkfp2.DBMatch(mDBHandle, CapTmp, RegTmp);
+                                if (0 < ret)
+                                {
+                                    textRes.Text = "Match finger succ, score=" + ret + "!";
+                                    return;
+                                }
+                                else
+                                {
+                                    textRes.Text = "Match finger fail, ret= " + ret;
+                                    return;
+                                }
+                            }
+                        }
+                    }
+                    break;
+
+                default:
+                    base.DefWndProc(ref m);
+                    break;
+            }
+        }
+
+        private void CloseDevice()
+        {
+            if (IntPtr.Zero != mDevHandle)
+            {
+                bIsTimeToDie = true;
+                Thread.Sleep(1000);
+                captureThread.Join();
+                zkfp2.CloseDevice(mDevHandle);
+                mDevHandle = IntPtr.Zero;
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            FormHandle = this.Handle;
+        }
+
+        private void bnIdentify_Click(object sender, EventArgs e)
+        {
+            if (!bIdentify)
+            {
+                bIdentify = true;
+                textRes.Text = "Please press your finger!";
+            }
+        }
+
+        private void bnClose_Click(object sender, EventArgs e)
+        {
+            CloseDevice();
+            RegisterCount = 0;
+            Thread.Sleep(1000);
+            bnInit.Enabled = false;
+            bnFree.Enabled = true;
+            bnOpen.Enabled = true;
+            bnClose.Enabled = false;
+            bnEnroll.Enabled = false;
+            bnVerify.Enabled = false;
+            bnIdentify.Enabled = false;
+        }
+
+        private void bnVerify_Click(object sender, EventArgs e)
+        {
+            if (bIdentify)
+            {
+                bIdentify = false;
+                textRes.Text = "Please press your finger!";
+            }
+        }
+
+        private void bnEnroll_Click(object sender, EventArgs e)
+        {
+            if (!IsRegister)
+            {
+                IsRegister = true;
+                RegisterCount = 0;
+                cbRegTmp = 0;
+                textRes.Text = "Please press your finger 3 times!";
+            }
+        }
+
+
+
+        private void bnInit_Click(object sender, EventArgs e)
+        {
+            cmbIdx.Items.Clear();
+            int ret = zkfperrdef.ZKFP_ERR_OK;
+            if ((ret = zkfp2.Init()) == zkfperrdef.ZKFP_ERR_OK)
+            {
+                int nCount = zkfp2.GetDeviceCount();
+                if (nCount > 0)
+                {
+                    for (int i = 0; i < nCount; i++)
+                    {
+                        cmbIdx.Items.Add(i.ToString());
+                    }
+                    cmbIdx.SelectedIndex = 0;
+                    bnInit.Enabled = false;
+                    bnFree.Enabled = true;
+                    bnOpen.Enabled = true;
+                }
+                else
+                {
+                    zkfp2.Terminate();
+                    MessageBox.Show("No device connected!");
+                }
+            }
+            else
+            {
+                MessageBox.Show("Initialize fail, ret=" + ret + " !");
+            }
+        }
+
+        private void bnFree_Click(object sender, EventArgs e)
+        {
+            zkfp2.Terminate();
+            cbRegTmp = 0;
+            bnInit.Enabled = true;
+            bnFree.Enabled = false;
+            bnOpen.Enabled = false;
+            bnClose.Enabled = false;
+            bnEnroll.Enabled = false;
+            bnVerify.Enabled = false;
+            bnIdentify.Enabled = false;
+        }
+
+        private void bnOpen_Click(object sender, EventArgs e)
+        {
+            int ret = zkfp.ZKFP_ERR_OK;
+            if (IntPtr.Zero == (mDevHandle = zkfp2.OpenDevice(cmbIdx.SelectedIndex)))
+            {
+                MessageBox.Show("OpenDevice fail");
+                return;
+            }
+            if (IntPtr.Zero == (mDBHandle = zkfp2.DBInit()))
+            {
+                MessageBox.Show("Init DB fail");
+                zkfp2.CloseDevice(mDevHandle);
+                mDevHandle = IntPtr.Zero;
+                return;
+            }
+            bnInit.Enabled = false;
+            bnFree.Enabled = true;
+            bnOpen.Enabled = false;
+            bnClose.Enabled = true;
+            bnEnroll.Enabled = true;
+            bnVerify.Enabled = true;
+            bnIdentify.Enabled = true;
+            btnClear.Enabled = true;
+            btnAddFinger.Enabled = true;
+            RegisterCount = 0;
+            cbRegTmp = 0;
+            iFid = 1;
+            for (int i = 0; i < 3; i++)
+            {
+                RegTmps[i] = new byte[2048];
+            }
+            byte[] paramValue = new byte[4];
+            int size = 4;
+            zkfp2.GetParameters(mDevHandle, 1, paramValue, ref size);
+            zkfp2.ByteArray2Int(paramValue, ref mfpWidth);
+
+            size = 4;
+            zkfp2.GetParameters(mDevHandle, 2, paramValue, ref size);
+            zkfp2.ByteArray2Int(paramValue, ref mfpHeight);
+
+            FPBuffer = new byte[mfpWidth * mfpHeight];
+            FPBufferTest = new byte[mfpWidth * mfpHeight];
+
+            captureThread = new Thread(new ThreadStart(DoCapture));
+            captureThread.IsBackground = true;
+            captureThread.Start();
+            bIsTimeToDie = false;
+            textRes.Text = "Open succ";
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            //for (int i = 0; i < iFid; i++)
+            //{
+            //    zkfp2.DBDel(mDBHandle, i);
+            //}
+
+            if(zkfp2.DBClear(mDBHandle) == zkfp.ZKFP_ERR_OK)
+            {
+                iFid = 1;
+                textRes.Text = "Cleared all fingerprint!";
+            }
+        }
+
+        /// <summary>
+        /// dung de test.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btnAddFinger_Click(object sender, EventArgs e)
+        {
+            //Cong
+            FPBufferTest = zkfp2.Base64ToBlob("Sp9TUzIxAAAD3OEECAUHCc7QAAAj3WoBAABkgwEjYtzgAPxkkAB+AAm4OADZAGZkmAAX3QFkrADYAEtkjtyVAA" +
+                            "lksgBkAIm4DgDkAOVkSQBM3VFSPQBzALVko9xaAVAzhwCXAPm4XwD9APpk+ADd3O9kfACtAMJkTNwOAVxkfgDkASy4xADcABlcZgAz3ThRXQBJARdhWdxv" +
+                            "APlkEwD2AU+4awBYAPhkbwBU3AJPSQDuAKRkodzsAItkjwDIARy4jQCxAIVkuQCW3HtkJgCxACpkK9wnAU1kMwBAAPK4wAA2ATg6/QBZ3TBFpwBgAQAo8k" +
+                            "PIBwoRKIfUBjXeQXpBByZu1KRC1pPRRQb5dlOG9ky4h9IEhoKjBl1fk4OX+G/6jAJJUA8aZYEJezp5S1LIAvYb/gRL+fDdyP7loiGb/Yq6UZf1hYQ5jlD+N" +
+                            "oAAZt79LRMnMWEm7I7Wkh+TlwChNtsOHqK3hEqlYt8Ai/ILHQKSHiAv4AOCAPeJwwn809L/xXU5awD/yiJPbeZ2vfP/lY02LX+FgL1/wHBKqjP0wotfAHoB" +
+                            "nt4f82seZYG/+9zY0wCjdKfp+2LwOgbmVHZWdL6MsGvm3QMgOAHHFRm0BgB8RW1KlwQDSEt3awUAo5J9ZtoBZFtxwFLOAMK7h/95Tl0KxV1tKz89XAMAO7d" +
+                            "0wtgBPXdtPQ3FNIAx/j7/REwOxTaLKFL//v9dN8YALVV1wgcAkJLDN1nXAXmTfWnBBcP9pwIAkJgMwcIAeUV7am0RANlTkMOiwYFpWcASxduiTMFqwsH+ww" +
+                            "XAfR2ABgCTsgaHwAjcirSDeMLBosAF3JO5Bv5bC8WKv1rDwXjCWQfFkr3QREsJAKvWVW+LHRAAwNmTagTBih1dwXAJAKgfifwdkGAGADfconjD2QGw3BbAN" +
+                            "9YAAArd//1CRsD1wTsdEwDUuoxiBP/AtnWEwAsAYiX3/PT+OMAHADomZ8IdwGYHAE7sKP04IwgARu1nwb3Awx4KAGH59/47/y3hCABi/vr+O//9Iy8KEFoA" +
+                            "awWXdhwEEIsKkKDQENTYn2nBwWyJOolnHQYQkwwX/vb+C8xND2SSgAjVkhL8/jAgCBBP1mCRHIwKEGET+jn9ICA/ChB4HIkBxcAfwMPEwQQQRSAjwRIQ1Rug" +
+                            "wavCch7Dwm7C/8LCEH34LPz//DUF1WkkuMXEwQYQKe1QdB4EEHEsOiHBEG/vSDoEEKUz9S0HzBQ1SWAWEAglrh9ukMGdwMIH/8FVwRIQoVbDR5LHGMLDwJef" +
+                            "DtVZa2zB/CT5+Pnm/VGeARJDAQEBxWwC7kwAABJFUg==");
+            //zkfp2.DBMerge(mDBHandle, FPBufferTest, FPBufferTest, FPBufferTest, RegTmp, ref cbRegTmp);
+            if (zkfp2.DBAdd(mDBHandle, iFid, FPBufferTest) == zkfp.ZKFP_ERR_OK)
+            {
+                iFid += 1;
+            }
+            //dat
+            FPBufferTest = zkfp2.Base64ToBlob("SxtTUzIxAAACWFsECAUHCc7QAAAaWWkBAAAAgoUKf1h7AAcOkQC4AIVWeACeAIEPkgCqWP4PwgC2AOYPbVjWAHoPMgAvAF" +
+                "ZXyAD1ACsPpwD/WFoORwAgAfoPg9m/idcLgYGTCmHTd4XGfdr+s4Gt3V77m/sDnyqnVHq2fc/7P/maFyGHEgvns9fTM+KAR7YJHufz79LHRlghLQEBwBVqBQL5GBDAW" +
+                "QcAaSgNHz8JAJ0wBjvA/WbBCwCVOAk6RlVvDQCDQQbAOv7CmEdCDQCES8Y1w25AwQQAQ2AoKwlYlVgJPv//BP4/XAFCaPRBCcWQYFTBwPzC/v0ECgLYawZA/0bAwQAw" +
+                "2vE4CgBwdTj//gb+UwQAMog/wv1TAWx+A/7BOv5CDwUAMpPwOM4Abt8H/0Y3QgfFdZjbeHMRADKeLv45psHB/f/+/zpdCVh8nwn/Kf4EwEBdAWOgd28NxXCqWPz//URRR" +
+                "MEAx+EhRgUAK8Ei/ytVAXCvBv7+OjXCpsBKBgAi1Rv+/WkNAGi3AyoF/UgcCQBq1n3DB8OGXgAG2T1M/8AAJYPhMw4AZL7G/cKkwf3B/sHAOn8TWDTn3v7+/j7+K6fB/sH" +
+                "AbxTFGe6vxv7///39O///d01zBAAy7pZ9BljK+SlGCQCb/2CcnMGDCRBoxxf5HcBsCBBmCeH9ZCsGEFoNTMS0wgpIYA5GicLBBAcSPxc3aYMGEIMjQibDFhAwPcD4Mf+kw" +
+                "fn+/v//BcDCmWsVEDtOt/D//Kb8/vz6/S+hbxZIRl20//8wOvz/o/j+/v/BwARl");
+            //zkfp2.DBMerge(mDBHandle, FPBufferTest, FPBufferTest, FPBufferTest, RegTmp, ref cbRegTmp);
+            if (zkfp2.DBAdd(mDBHandle, iFid, FPBufferTest) == zkfp.ZKFP_ERR_OK)
+            {
+                iFid += 1;
+            }
+            cbRegTmp = 100;
+
+            //int fid1 = 0, score1 = 0;
+
+            //var _res = zkfp2.DBIdentify(mDBHandle, CapTmp, ref fid1, ref score1);
+        }
+    }
+}
